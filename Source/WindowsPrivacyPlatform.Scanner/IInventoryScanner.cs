833d75c290fa1a32dcf3c7ea2fb706a928f020b4
HighLionNet <tortman25@gmail.com>
2026-08-09 13:55:17 +0300

    v2.1: IInventoryScanner supports CancellationToken

--- Source/WindowsPrivacyPlatform.Scanner/IInventoryScanner.cs
+++ Source/WindowsPrivacyPlatform.Scanner/IInventoryScanner.cs
@@ -1,8 +1,11 @@
-namespace WindowsPrivacyPlatform.Scanner;
-
+// Source/WindowsPrivacyPlatform.Scanner/IInventoryScanner.cs
+using System.Threading;
 using WindowsPrivacyPlatform.Models;
+
+namespace WindowsPrivacyPlatform.Scanner;
 
 public interface IInventoryScanner
 {
     ScanResult Scan();
+    ScanResult Scan(CancellationToken cancellationToken);
 }
